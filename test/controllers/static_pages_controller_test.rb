7dcc813b7488e76a2e554bef31f7ecef1f82877f
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-20 02:09:51 +0000

    Add contact page, dry code

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Twitteresque"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
@@ -16,13 +20,20 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
   
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+  
   test "titles should be correct" do
     get :home
-    assert_select "title", "Home | Twitter Clone"
+    assert_select "title", "Home | #{@base_title}"
     get :about
-    assert_select "title", "About | Twitter Clone"
+    assert_select "title", "About | #{@base_title}"
     get :help
-    assert_select "title", "Help | Twitter Clone"
+    assert_select "title", "Help | #{@base_title}"
+    get :contact
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
