1d8e930afa5b268e3ba22ef8aa2a986803766290
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-20 01:56:13 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,5 +10,19 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "titles should be correct" do
+    get :home
+    assert_select "title", "Home | Twitter Clone"
+    get :about
+    assert_select "title", "About | Twitter Clone"
+    get :help
+    assert_select "title", "Help | Twitter Clone"
+  end
 
 end
