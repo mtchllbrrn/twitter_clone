c7221f8f7dc30c9c4f35d34ca556699be3314220
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-28 17:55:59 +0000

    Add title to signup page, include test for title'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+  def setup
+    @base_title = 'Twitteresque'
+  end
+  
+  test 'should get new' do
     get :new
     assert_response :success
   end
-
+  
+  test 'titles should be correct' do
+    get :new
+    assert_select 'title', "Sign Up | #{@base_title}"
+  end
 end
