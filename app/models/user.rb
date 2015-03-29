ed73ea71dd6e49041ed3a4a4237a463ebe6e2f8c
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-29 16:31:04 +0000

    Add user validations, tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
+  
+  before_save { self.email = email.downcase }
+  
+  validates :name,  presence: true,
+                    length: { minimum: 3, maximum: 50 }
+                    
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { minimum: 3, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,41 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test 'name should be within length range' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+    @user.name = 'aa'
+    assert_not @user.valid?
+  end
+  
+  test 'email should be within length range' do
+    @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+    @user.email = 'a@'
+    assert_not @user.valid?
+  end
+  
+  test 'should accept valid emails' do
+    valid_emails = %w[user@example.com USER@foo.example.com foo.bar@example.com
+                      foo-bar+baz@example.qux.com foo_bar@example.com]
+    valid_emails.each do |valid_email|
+      @user.email = valid_email
+      assert @user.valid?, "#{valid_email.inspect} should be valid"
+    end
+  end
+  
+  test 'should reject invalid emails' do
+    invalid_emails = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_emails.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "#{invalid_email.inspect} should be invalid"
+    end
+  end
+  
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
