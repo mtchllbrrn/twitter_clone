f679bad9aa19b32337ac5f1f87542869f4cb6c8d
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-31 23:33:05 +0000

    Generate sessions controller, implement simple login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in, redirect to profile page
+    else
+      flash[:danger] = "Invalid email/password combination" # Not quite right.
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+end
