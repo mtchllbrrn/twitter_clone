080aab0a1cc21cfe6dd1ebca8cbfd285a21f06ed
Mitchell Barron <mitchell.stocky@gmail.com>
2015-03-31 02:50:38 +0000

    Implement user signup integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,12 +4,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test 'invalid signup should not add to db' do
     get signup_path
-    assert_no_difference User.count do
+    assert_no_difference 'User.count' do
       post users_path user: { name: "",
                               email: "bogus@example",
                               password: "bogus",
                               password_confirmation: "morebogus" }
       assert_template 'users/new'
     end
+  end
+  
+  test 'valid signup should add user to db' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Test User",
+                                            email: "test@example.com",
+                                            password: "foobar",
+                                            password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
   end
 end
