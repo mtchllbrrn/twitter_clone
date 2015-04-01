05b0529de7b25a5801dda51aee5c90e5d6d64e47
Mitchell Barron <mitchell.stocky@gmail.com>
2015-04-01 02:27:24 +0000

    Add user sign-in integration tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+  
   test 'on invalid sign in, flash should only show on one page' do
     get login_path
     assert_template 'sessions/new'
@@ -11,5 +15,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert flash.empty?, 'Flash persisted after invalid sign-in'
+  end
+  
+  test 'login with valid information' do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', logout_path
   end
 end
