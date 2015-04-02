580312c21dda81e01f015643092e8e0f1543f2f1
Mitchell Barron <mitchell.stocky@gmail.com>
2015-04-02 18:41:53 +0000

    Implement user edit pages w/ tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
                     length: { minimum: 3, maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    
-  validates :password, length: { minimum: 6 }                  
-                    
+                   
+  validates :password, length: { minimum: 6 }, allow_blank: true
+                   
   has_secure_password
   
   # Returns the hash digest of the given string.
