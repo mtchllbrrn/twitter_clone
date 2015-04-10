e52123ed073ee32ce205e2e0a5839eb6239b0dac
Mitchell Barron <mitchell.stocky@gmail.com>
2015-04-10 04:02:07 +0000

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
-  
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -19,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) #Not the final implementation
     if @user.save
-      log_in(@user)
-      flash[:success] = 'Welcome to Twitteresque!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
     else
       render 'new'
     end
